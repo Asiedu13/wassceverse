f1d686a5d8375e47dfed5178898e705a901d0ba4
Prince Asiedu <princekofasiedu@gmail.com>
2024-01-20 13:19:56 +0000

    refactor: Created a layout file for the student directory

--- app/(onboarding)/student/indexnumandschool/page.js
+++ app/(onboarding)/student/indexnumandschool/page.js
@@ -1,27 +1,10 @@
 import { IndexNumForm } from "../../../components/onboarding/IndexNumForm";
-import { CustomProgressBar } from "../../../components/onboarding/CircularProgress";
 
 export default function Page() {
   
   return (
     <section className="flex flex-col justify-between h-[90vh] w-[90vw] overflow-clip">
       <section>
-        <nav className="flex justify-between items-center p-[20px]">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="10.31"
-            height="18.034"
-            viewBox="0 0 10.31 18.034"
-          >
-            <path
-              id="Icon_ionic-ios-arrow-back"
-              data-name="Icon ionic-ios-arrow-back"
-              d="M14.359,15.208,21.182,8.39a1.289,1.289,0,0,0-1.825-1.82L11.626,14.3a1.286,1.286,0,0,0-.038,1.777l7.763,7.779a1.289,1.289,0,1,0,1.825-1.82Z"
-              transform="translate(-11.251 -6.194)"
-            />
-          </svg>
-          <CustomProgressBar />
-        </nav>
         <header className="my-[30px] px-[20px] w-[90vw]">
           <h1 className="text-3xl mb-[20px] w-[300px] ">
             Select your School and Input your Index Number
