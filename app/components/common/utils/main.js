c8f642426b2243dc79e1eab6ece88d5bef82372d
Prince Asiedu <princekofasiedu@gmail.com>
2024-01-20 14:45:26 +0000

    chore: moved file

--- app/components/common/utils/main.js
+++ app/components/common/utils/main.js
@@ -2,6 +2,7 @@ export function saveDataInLocalStorage(storageName, value) {
     if ( storageName.length > 0 && value.length > 0 ) {
         localStorage.setItem( storageName, value );
     }
+    console.log( localStorage.getItem( storageName ) );
 }
 
 function resetDataValues() {
