73b8c9ca007c9a5143f048e209274836f697f99d
Prince Asiedu <princekofasiedu@gmail.com>
2024-01-21 16:06:56 +0000

    refactor: Created a general function for storing the form data

--- app/components/common/utils/main.js
+++ app/components/common/utils/main.js
@@ -1,10 +1,21 @@
 export function saveDataInLocalStorage(storageName, value) {
-    if ( storageName.length > 0 && value.length > 0 ) {
-        localStorage.setItem( storageName, value );
-    }
-    console.log( localStorage.getItem( storageName ) );
+  if (storageName.length > 0 && value.length > 0) {
+    localStorage.setItem(storageName, value);
+  }
+  console.log(localStorage.getItem(storageName));
 }
 
 function resetDataValues() {
-    // do something
+  // do something
+}
+
+export function getAndVerifyData(event, inputLabel, inputValue) {
+  event.preventDefault();
+
+  if (inputValue !== "" && inputValue != undefined) {
+    console.log(`${inputLabel}`);
+    saveDataInLocalStorage(inputLabel, inputValue);
+    return true;
+  }
+  return false;
 }
