d6226056e65bfa02e676a592958c2197c0d28040
Prince Asiedu <princekofasiedu@gmail.com>
2024-01-26 16:04:34 +0000

    chore: dummy data for electives

---
+++ app/components/common/data/electives.js
@@ -0,0 +1,18 @@
+export const electives = [
+  {
+    "course": "General Science",
+    "checked": false
+  },
+  {
+    "course": "General Arts",
+    "checked": false
+  },
+  {
+    "course": "Business",
+    "checked": false
+  },
+  {
+    "course": "Home Economics",
+    "checked": false
+  }
+]
