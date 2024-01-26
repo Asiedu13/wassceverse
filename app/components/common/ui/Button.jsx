9ee942affb7f6f56ddd3c57b7bbe33d3356dd6a2
Prince Asiedu <princekofasiedu@gmail.com>
2024-01-26 16:04:04 +0000

    style: changed button width

--- app/components/common/ui/Button.jsx
+++ app/components/common/ui/Button.jsx
@@ -1,8 +1,8 @@
 export function Button({ label, variant = "primary" }) {
   const primaryStyle =
-    "bg-secondary text-white rounded-3xl text-lg p-3 w-[90vw]";
+    "bg-secondary text-white rounded-3xl text-lg p-3 w-[80vw]";
   const secondaryStyle =
-    "bg-semi-background border border-secondary text-secondary rounded-3xl text-lg p-3 w-[90vw]";
+    "bg-semi-background border border-secondary text-secondary rounded-3xl text-lg p-3 w-[80vw]";
   return (
     <button className={variant == "secondary" ? secondaryStyle : primaryStyle}>
       {label}
