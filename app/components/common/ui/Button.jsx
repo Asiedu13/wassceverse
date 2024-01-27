39d94b60d36cad98473f83f824de706e15190be4
Prince Asiedu <princekofasiedu@gmail.com>
2024-01-27 13:44:20 +0000

    style: added margin-bottom to button

--- app/components/common/ui/Button.jsx
+++ app/components/common/ui/Button.jsx
@@ -1,6 +1,6 @@
 export function Button({ label, variant = "primary" }) {
   const primaryStyle =
-    "bg-secondary text-white rounded-3xl text-lg p-3 w-[80vw]";
+    "bg-secondary text-white rounded-3xl text-lg p-3 w-[80vw] mb-[10px]";
   const secondaryStyle =
     "bg-semi-background border border-secondary text-secondary rounded-3xl text-lg p-3 w-[80vw]";
   return (
